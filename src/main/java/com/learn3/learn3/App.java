980ef461b4b8d6ea06255ef89ae74f7ea832f04c
BodduVenkata S <bodduvenkata.s@mphasis.com>
2025-02-03 09:43:20 +0530

    my first commit

---
+++ src/main/java/com/learn3/learn3/App.java
@@ -0,0 +1,14 @@
+package com.learn3.learn3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello santosh");
+        
+    }
+}
